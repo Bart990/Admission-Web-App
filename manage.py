691dfaa99c0b372cd681e6f18f2e5df30b963ba2
Bart <artembo14545@icloud.com>
2025-05-26 01:40:10 +0300

    переделываем на мифи

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import sys
 
 def main() -> None:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'student_portal.settings')
+                          'mephi_app.settings')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
--- student_portal/wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
-WSGI-точка входа, генерируется стандартной командой django-admin startproject
-"""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "student_portal.settings")
-application = get_wsgi_application()
